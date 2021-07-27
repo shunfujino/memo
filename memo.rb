71f67b4a78bc847aac6cb86f53f2634020d95d7a
EC2 Default User <ec2-user@ip-172-31-28-163.us-east-2.compute.internal>
2021-07-27 14:22:55 +0000

    1,新規ファイル作成時に内容も記述するように変更。2,ファイルの内容に追記できるように変更。 exit

--- memo.rb
+++ memo.rb
@@ -8,17 +8,19 @@ when "1" # 新規メモ作成
 puts "新規でメモを作成します"
     puts "ファイル名を入力してください。(拡張子は不要)"
     memo_title = gets.chomp.to_s
+    puts "ファイルの内容を入力"
+    memo_content = gets.chomp.to_s 
     CSV.open(memo_title,'w')do|csv|
-    csv << []
+    csv << [memo_content]
     end
     
 when "2"  # 既存のメモを編集
     puts "既存のメモを編集します"
     puts "編集したいメモのファイル名を入力。（拡張子不要）"
     memo_title = gets.chomp.to_s
-    puts "内容を入力"
+    puts "内容に追記"
     memo_content = gets.chomp.to_s 
-    CSV.open(memo_title,'w')do|csv|
+    CSV.open(memo_title,'a')do|csv|
     csv << [memo_content]
     end
 else
